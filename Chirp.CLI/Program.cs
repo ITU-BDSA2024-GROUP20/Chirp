5eff81ea9c412ef593abe0c059f26e99ac5c4ad7
SifDJ <due.jacobsen@gmail.com>
2024-08-28 15:47:55 +0200

    Half Finished Version of First Iteration
    It does not work, but it is a start. Feel free to overwrite.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args[0] == "read")
+{
+    try
+    {
+        using StreamReader reader = new StreamReader(@"chirp_cli_db.csv");
+        string text = reader.ReadToEnd();
+        string[] fullMessage = text.Split('\n');
+        Post[] posts = new Post[fullMessage.Length];
+        for (int i = 0; i < fullMessage.Length; i++)
+        {
+            posts[i] = new Post(fullMessage[i]);
+            Console.WriteLine(posts[i].getPost());
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("This file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+
+class Post
+{
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public string Timestamp { get; set; }
+
+    public Post(String fullMessage)
+    {
+        int index1 = fullMessage.IndexOf(",");
+        Author = fullMessage.Substring(0, index1);
+        Message = fullMessage.Substring(index1+1);
+        int index2 = Message.LastIndexOf(",");
+        Timestamp = Message.Substring(index2);
+    }
+    public Post(String Author, String Message, String Timestamp)
+    {
+        this.Author = Author;
+        this.Message = Message;
+        this.Timestamp = Timestamp;
+    }
+
+    public string getPost()
+    {
+        return Author + " @ " + Timestamp + ": " + Message;
+    }
+}

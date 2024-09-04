82f406cf1881dc195f27ab26c8ef71a5f4c8ce33
SifDJ <due.jacobsen@gmail.com>
2024-09-04 14:58:23 +0200

    Added a CSVDatabase file to SimpleDB
    It only throws erros, but it sure can be called!

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDB;
+
+public class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
+    }
+}

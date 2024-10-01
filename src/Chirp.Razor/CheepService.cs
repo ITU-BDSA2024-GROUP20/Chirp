c85c4e2d3a9dc833557995f9a8ce6187c36bf119
SifDJ <due.jacobsen@gmail.com>
2024-10-01 15:52:15 +0200

    Filled in DBFacade to be easily usable by outside interference
    It has a proper constructor and functions. Cannot prove that they work though.

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -11,34 +11,20 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
-   
-
+    private readonly DBFacade facade;
+    
+    public CheepService(DBFacade facade){
+        this.facade = facade
+    }
 	
     public List<CheepViewModel> GetCheeps()
     {
-        List<CheepViewModel> cheeps = new List<CheepViewModel>();
-        var sqlDBFilePath = "chirp.db";
-        var sqlQuery = @"SELECT u.username, m.text, m.pub_date FROM message m JOIN user u ON m.author_id = u.user_id ORDER by m.pub_date desc";
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                var dataRecord = (IDataRecord)reader;
-                cheeps.Add(
-                    new CheepViewModel(
-                        dataRecord[0].ToString(),
-                        dataRecord[1].ToString(), 
-                        UnixTimeStampToDateTimeString(Double.Parse(dataRecord[2].ToString()))));
-                       
-            }
-        }
-        return cheeps;
+        string query = 
+            @"SELECT u.username, m.text, m.pub_date
+            FROM message m
+            JOIN user u ON m.author_id = u.user_id
+            ORDER by m.pub_date desc";
+        return facade.query(query);
     }
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,6 +1,35 @@
 namespace Chirp.Razor;
+using Microsoft.Data.Sqlite;
 
 public class DBFacade
 {
+    string path = "chirp.db";
     
+    public DBFacade(string path)
+    {
+        this.path = path;
+    }
+
+    public SqliteConnection establishConnection()
+    {
+        return new SqliteConnection($"Data Source={path}");
+    }
+    
+    public List<CheepViewModel> query(string query){
+        List<CheepViewModel> cheeps = new List<CheepViewModel>();
+        using (var connection = new SqliteConnection(establishConnection()){
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            using var reader = command.ExecuteReader();
+            while (reader.Read()){
+                var dataRecord = (IDataRecord)reader;
+                cheeps.Add(
+                    new CheepViewModel(
+                        dataRecord[0].ToString(),
+                        dataRecord[1].ToString(), 
+                        UnixTimeStampToDateTimeString(Double.Parse(dataRecord[2].ToString()))));
+            }
+        }
+    }
 }

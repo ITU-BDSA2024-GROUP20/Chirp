64172cb8547844068080bdab71263595a2304ff1
SifDJ <due.jacobsen@gmail.com>
2024-11-12 12:42:17 +0100

    Added DeleteCheep and DeleteCheepsByAuthor

--- src/Chirp.Infrastructure/ICheepRepository.cs
+++ src/Chirp.Infrastructure/ICheepRepository.cs
@@ -7,6 +7,8 @@ public interface ICheepRepository
     public void CreateCheep(CheepDTO newCheep);
     public List<CheepDTO> ReadCheep( int page , string userName);
     public void UpdateCheep(CheepDTO alteredCheep);
+    public void DeleteCheep(int cheepId);
+    public void DeleteCheepsByAuthor(string authorId);
     public Author GetAuthorByName(string name);
     public Author GetAuthorByEmail(string email);
 }

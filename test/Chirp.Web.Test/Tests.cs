f06ff54b20feae9a4b23aded2204219cb42d47db
SifDJ <due.jacobsen@gmail.com>
2024-11-19 15:32:21 +0100

    Updated Tests to be up to date with UI
    Thamks

--- test/Chirp.Web.Test/Tests.cs
+++ test/Chirp.Web.Test/Tests.cs
@@ -6,18 +6,29 @@ using Microsoft.Playwright;
 public class Tests : PageTest
 {
     [Test]
-    public async Task MyTest()
+    public async Task TestRegister()
+    {
+        await Page.GotoAsync("http://localhost:5273/");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+        await Page.GetByPlaceholder("username").ClickAsync();
+        await Page.GetByPlaceholder("username").FillAsync("SifDJ");
+        await Page.GetByPlaceholder("username").PressAsync("Tab");
+        await Page.GetByPlaceholder("name@example.com").FillAsync("sif@gmail.com");
+        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("HellaC00!");
+        await Page.GetByLabel("Password", new() { Exact = true }).PressAsync("Tab");
+        await Page.GetByLabel("Confirm Password").FillAsync("HellaC00!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+    }
+    [Test]
+    public async Task TestLogin()
     {
         await Page.GotoAsync("http://localhost:5273/");
         await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync("ropf@itu.dk");
-        await Page.GetByPlaceholder("name@example.com").PressAsync("Tab");
+        await Page.GetByPlaceholder("email/username").ClickAsync();
+        await Page.GetByPlaceholder("email/username").FillAsync("Helge");
+        await Page.GetByPlaceholder("email/username").PressAsync("Tab");
         await Page.GetByPlaceholder("password").FillAsync("LetM31n!");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Textbox).ClickAsync();
-        await Page.GetByRole(AriaRole.Textbox).FillAsync("Wow");
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
-        
     }
 }
